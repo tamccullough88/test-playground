9d5d3a52e583feafd437d4156d6116c5596fbd0f
tamccullough88 <tamccullough88@gmail.com>
2023-12-04 20:19:36 -0500

    set up test to show when color changes, check box enabled, and h1 added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import { useState } from "react"
 
 function App() {
+  const [btnColor, setBtnColor] = useState("blue")
+  const newBtnColor = btnColor === "blue" ? "green" : "blue"
+  const [inputDisabled, setInputDisabled] = useState(false)
+  const [buttonStatus, setButtonStatus] = useState("Button is disabled")
+  const newButtonStatus = buttonStatus === "Button is disabled" ? "Button is enabled" : "Button is disabled"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing Playground</h1>
+      <p role="paragraph">{newButtonStatus}</p>
+      <button
+        style={{ backgroundColor: btnColor }}
+        onClick={() => setBtnColor(newBtnColor)}
+        disabled={inputDisabled}
+      >
+        Change button color to {newBtnColor}
+      </button>
+
+      <input
+        type="checkbox"
+        defaultChecked={inputDisabled}
+        onChange={(event) => setInputDisabled(event.target.checked)}
+        onClick={() => setButtonStatus(newButtonStatus)}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
